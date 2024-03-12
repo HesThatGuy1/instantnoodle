51fa44ffc1487f522b8fdacea947671a16e42616
hesthatguy1 <jordan.savior15@gmail.com>
2024-03-12 00:16:45 -0400

    fix release script

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export ARCH=arm64 && export SUBARCH=arm64
 
-mv /home/hesthatguy/Android/kernel_sources/instantnoodle/newbuild3/out/arch/arm64/boot/Image /home/hesthatguy/Android/kernel_sources/instantnoodle/newbuild3/out/arch/boot.img-zImage
-mv /home/hesthatguy/Android/kernel_sources/instantnoodle/newbuild3/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb /home/hesthatguy/Android/kernel_sources/instantnoodle/newbuild3/out/arch/boot.img-dtb
+mv /home/hesthatguy/Android/kernel_sources/instantnoodle/2024Kernel/out/arch/arm64/boot/Image /home/hesthatguy/Android/kernel_sources/instantnoodle/2024Kernel/out/arch/boot.img-zImage
+mv /home/hesthatguy/Android/kernel_sources/instantnoodle/2024Kernel/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb /home/hesthatguy/Android/kernel_sources/instantnoodle/2024Kernel/out/arch/boot.img-dtb
