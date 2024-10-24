93be3fa4f34fe4bbca4a6ba1e3eb79a7a8e1a667
hesthatguy1 <jordan.savior15@gmail.com>
2024-10-24 07:00:06 -0400

    add build, release, & clean up scripts

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+export ARCH=arm64 && export SUBARCH=arm64
+
+PATH=${PATH}:~/Android/toolchains/AOSP/Clang/clang-r383902c/bin
+export CLANG_TRIPLE=aarch64-linux-gnu-
+
+export PATH=${PATH}:~/Android/toolchains/AOSP/GCC/aarch64-linux-android-4.9/bin
+export CROSS_COMPILE=aarch64-linux-android-
+
+export PATH=${PATH}:~/Android/toolchains/AOSP/GCC/arm-linux-androideabi-4.9/bin
+export CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+make O=out clean && make O=out mrproper
+make O=out custom_defconfig
+time make -j$(nproc --all) O=out CC=clang
---
+++ release.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export ARCH=arm64 && export SUBARCH=arm64
+
+mv /home/hesthatguy/Android/kernel_sources/instantnoodle/Android_10/out/arch/arm64/boot/Image /home/hesthatguy/Android/kernel_sources/instantnoodle/Android_10/out/arch/boot.img-zImage
+mv /home/hesthatguy/Android/kernel_sources/instantnoodle/Android_10/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb /home/hesthatguy/Android/kernel_sources/instantnoodle/Android_10/out/arch/boot.img-dtb
