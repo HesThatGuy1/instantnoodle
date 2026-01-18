9edf297e277b49f1f5942e3444ba32da99658d0a
hesthatguy1 <jordan.savior15@gmail.com>
2026-01-18 07:08:58 -0500

    various changes

--- build.sh
+++ build.sh
@@ -15,19 +15,19 @@ export SUBARCH=arm64
 PATH=${PATH}:~/Android/toolchains/AOSP/binutils-aarch64/bin
 # PATH=${PATH}:~/Android/toolchains/AOSP/binutils-arm/bin
 
-# export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-gnu-
 # export CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
 
 
 	# Toolchains
 	# Note: kernel will compile with Clang pathway disabled
 PATH=${PATH}:~/Android/toolchains/AOSP/Clang/clang-r383902c/bin
-PATH=${PATH}:~/Android/toolchains/AOSP/GCC/aarch64-linux-android-4.9/bin
-PATH=${PATH}:~/Android/toolchains/AOSP/GCC/arm-linux-androideabi-4.9/bin
+# PATH=${PATH}:~/Android/toolchains/AOSP/GCC/aarch64-linux-android-4.9/bin
+# PATH=${PATH}:~/Android/toolchains/AOSP/GCC/arm-linux-androideabi-4.9/bin
 
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=aarch64-linux-android-
-export CROSS_COMPILE_ARM32=arm-linux-androideabi-
+# export CROSS_COMPILE=aarch64-linux-android-
+# export CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
 
 	# Build in Out folder
--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -195,13 +195,13 @@ unsigned int sysctl_sched_cfs_bandwidth_slice		= 2000UL;
  * The margin used when comparing utilization with CPU capacity:
  * util * margin < capacity * 1024
  *
- * (default: ~20%)
+ * (default: ~12.5%)
  */
-unsigned int capacity_margin				= 1280;
+unsigned int capacity_margin				= 1152;
 unsigned int sched_capacity_margin_up[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 1078}; /* ~5% margin */
+			[0 ... NR_CPUS-1] = 1056}; /* ~3.125% margin */
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 1205}; /* ~15% margin */
+			[0 ... NR_CPUS-1] = 1120}; /* ~9.375% margin */
 
 #ifdef CONFIG_SCHED_WALT
 /* 0.25ms default for 8ms window size scaled to 1024 */
