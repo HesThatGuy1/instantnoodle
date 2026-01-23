04dd99a0bd1e5b53ff59088fff1e6c74df5ed772
hesthatguy1 <jordan.savior15@gmail.com>
2026-01-23 09:12:03 -0500

    add build, release, & clean scripts

---
+++ release.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export ARCH=arm64
+export SUBARCH=arm64
+
+mv /home/hesthatguy/Android/kernels/instantnoodle/Android11/base-8t/out/arch/arm64/boot/Image /home/hesthatguy/Android/kernels/instantnoodle/Android11/base-8t/out/arch/boot.img-zImage
+mv /home/hesthatguy/Android/kernels/instantnoodle/Android11/base-8t/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb /home/hesthatguy/Android/kernels/instantnoodle/Android11/base-8t/out/arch/boot.img-dtb
